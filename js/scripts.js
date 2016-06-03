0798bd20bb0bcc7d0bd557b01b0c3d9933171e0d
Benjamin Logue <benjamin.logue73@gmail.com>
2016-06-03 09:02:24 -0700

    added variables with gathered values of radio buttons into array called answers. created variable to act as counters to tally result. made for loop to iterate over array and add one to counter if answer matches value.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,33 @@ $(document).ready(function(){
     var q5 = $("input:radio[name=question5]:checked").val();
     var q6 = $("input:radio[name=question6]:checked").val();
 
+    var answer = [q1, q2, q3, q4, q5, q6];
+
+    var china = 0;
+    var mexico = 0;
+    var england = 0;
+    var canada = 0;
+    var germany = 0;
+
+    for (i = 0; i < 6; i++) {
+      if (answer[i] === "china1") {
+        china++;
+      } else if (answer[i] === "mexico1") {
+        mexico++;
+      } else if (answer[i] === "england1") {
+        england++;
+      } else if (answer[i] === "canada1") {
+        canada++;
+      } else if (answer[i] === "germany1") {
+        germany++;
+      }
+    }
+
+    console.log(china);
+    console.log(mexico);
+    console.log(england);
+    console.log(canada);
+    console.log(germany);
 
 
     event.preventDefault();
